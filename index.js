300efd0f772d3965dd42ccfcf76d8563fe328d30
slavikok <slavikokilka@gmail.com>
2023-04-09 03:00:00 +0300

    fix onCheck and delete func, add edit func

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 let button = document.getElementById("add-task");
-
 let list = document.getElementById("list");
 let todoInput = document.getElementById("title");
 let todoDesc = document.getElementById("desc");
 let title = document.getElementById("title-list");
-let isDone = document.getElementById("btn-cossout");
 
 list.addEventListener("click", onCheck);
 button.addEventListener("submit", addTask);
+list.addEventListener("click", deleteTask);
+list.addEventListener("click", editTask2);
 
 const baseData = [
   {
     id: 13134356,
-    title: "Wolk with dog",
+    title: "Walk with dog",
     desc: "cjvjvjv",
     isDone: false,
   },
@@ -25,8 +25,8 @@ function addTask(event) {
   //validation
   const newTask = {
     id: Math.floor(Math.random() * 10000),
-    title: todoInput.value, // empty   створити умову якщо цей елемент пустий то не показувати
-    desc: todoDesc.value, // empty і можна попробувати до 50 елем
+    title: todoInput.value,
+    desc: todoDesc.value,
     isDone: false,
   };
   baseData.push(newTask);
@@ -35,74 +35,63 @@ function addTask(event) {
 
   todoInput.value = "";
   todoDesc.value = "";
-  console.log(baseData);
 }
 
 function onCheck(event) {
+  if (event.target.dataset.action !== "check") return;
   const taskElement = event.target.closest(".list__task");
   const taskId = parseInt(taskElement.getAttribute("key"));
   const task = baseData.find((task) => task.id === taskId);
   console.log(task);
-
   task.isDone = !task.isDone;
-  const taskTitle = taskElement.querySelector(".list__task-title");
-  const taskDescription = taskElement.querySelector(".list__task-description");
   taskElement.classList.toggle("done");
-  // taskDescription.classList.toggle("done");
 }
 
-// function onCheck(event) {
-//   console.log("onCheck", event.target.id);
-//   event.preventDefault();
-//   console.log(event.target.dataset);
-//   if (event.target.dataset.action === "done") {
-//     const parentNode = event.target.closest(".list__task");
+function deleteTask(event) {
+  if (event.target.dataset.action !== "delete") return;
+  let taskTitle = event.target.closest(".list__task");
+  let taskId = parseInt(taskTitle.getAttribute("key"));
+  const taskIndex = baseData.findIndex((task) => task.id === taskId);
+  console.log(taskIndex);
+  baseData.splice(taskIndex, 1);
+  taskTitle.innerHTML = "";
+}
 
-//     const id = baseData[parentNode.id];
-//     const task = baseData.find((task) => task.id === id);
+function editTask2(event) {
+  if (event.target.dataset.action !== "edit") return;
+  let taskEl = document.querySelector(".list__task");
+  let taskId = parseInt(taskEl.getAttribute("key"));
+  let taskBase = baseData.find((task) => task.id === taskId);
+  let title = document.querySelector(".list__task-title");
+  let desc = document.querySelector(".list__task-description");
 
-//     task.done = !task.done;
+  title.contentEditable = true;
+  title.style.backgroundColor = "#e6e8ea";
+  desc.contentEditable = true;
+  desc.style.backgroundColor = "#e6e8ea";
 
-//     const taskTitle = parentNode.querySelector(".list__task-title");
-//     const taskDescription = parentNode.querySelector(".list__task-description");
-
-//     taskTitle.classList.toggle("done");
-//     taskDescription.classList.toggle("done");
-//   }
-// }
-
-// function actionsList(event) {
-//   item = event.target;
-//   let a = item.parentElement;
-//   let wholeLi = a.parentElement;
-//   console.log(item);
-//   if (item.classList[0] === "bin") {
-//     wholeLi.remove();
-//   }
-//   if (item.classList[0] === "bi-check2-circle") {
-//     wholeLi.classList.add("done");
-//   }
-//   if (item.classList[0] === "bi-pencil-square") {
-//     let input = document.createElement("input");
-//     wholeLi.appendChild(input);
-//   }
-// }
-
-// і щоб з бази даних міняло isDone відповідно до того як є на сайті
+  title.addEventListener("blur", () => {
+    taskBase.title = title.textContent;
+    taskBase.desc = desc.textContent;
+    render();
+  });
+  desc.addEventListener("blur", () => {
+    taskBase.title = title.textContent;
+    taskBase.desc = desc.textContent;
+    render();
+  });
+  console.log(baseData);
+}
 
 function render() {
   list.innerHTML = "";
   baseData.forEach((task) => {
     const renderTamplateTask = `<li class="list__task " key =${task.id}>
-    
-    <i class="check-true bi bi-check2-circle"  data-action="done" style="font-size: 23px" ></i>
-    </i>
-      <a href="#"
-         ><i class="bi-pencil-square bi" style="font-size: 23px"></i>
-      </a>
-      <a href="#"><i class="bin bi-trash bi" id="bin" style="font-size: 23px"></i> </a>
-      <div class="list__task-title " id="title-list">${task.title}</div>
-      <div class="list__task-description ">${task.desc}</div>
+    <i class="check-true bi bi-check2-circle" data-action="check" style="font-size: 23px" ></i>
+    <i class="bi-pencil-square bi" data-action="edit" style="font-size: 23px"></i>
+    <i class="bin bi-trash bi" data-action="delete" id="bin" style="font-size: 23px"></i>
+    <div class="list__task-title " id="title-list">${task.title}</div>
+    <div class="list__task-description ">${task.desc}</div>
     </li>`;
     list.innerHTML += renderTamplateTask;
   });
